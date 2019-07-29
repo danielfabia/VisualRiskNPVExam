6cb5a839d9535adf4fa0cb3ca08da58eb998c1af
Daniel Fabia <daniel.fabia@gmail.com>
2019-07-29 17:55:19 +0800

    Added boiler plate Web application

---
+++ WebApi/Models/ProfileModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    /// <summary>
+    /// ProfileModel
+    /// </summary>
+    public class ProfileModel
+    {
+        /// <summary>
+        /// Discount Rate
+        /// </summary>
+        public float Rate { get; set; }
+        /// <summary>
+        /// Net Present Value
+        /// </summary>
+        public decimal Npv { get; set; }
+    }
+}
