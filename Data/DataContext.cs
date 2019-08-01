da7b54b2e791b1a19214dc17dc8bc86c02649289
Daniel Fabia <daniel.fabia@gmail.com>
2019-08-01 10:37:58 +0800

    Added Data project for enttities and DbContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<NpvProfile> NpvProfiles { get; set; }
+    }
+}
---
+++ Data/Entities/NpvProfile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class NpvProfile
+    {
+        public int Id { get; set; }
+        public decimal InitialCost { get; set; }
+        public float LowerBoundRate { get; set; }
+        public float UpperBoundRate { get; set; }
+        public float RateIncrement { get; set; }
+        public ICollection<CashFlow> CashFlows { get; set; }
+    }
+}
