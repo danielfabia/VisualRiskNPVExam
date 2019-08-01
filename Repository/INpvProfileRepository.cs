d91e633e1f93e28c0f638ad55c39e971e862fc7a
Daniel Fabia <daniel.fabia@gmail.com>
2019-08-01 14:45:17 +0800

    Add the Repository project

---
+++ Repository/INpvProfileRepository.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface INpvProfileRepository
+    {
+        IEnumerable<NpvProfile> GetAll();
+        IEnumerable<NpvProfile> GetById(int id);
+        NpvProfile Add(NpvProfile newProfile);
+        NpvProfile Update(NpvProfile newProfile);
+    }
+}
---
+++ Repository/NpvProfileRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class NpvProfileRepository : INpvProfileRepository
+    {
+        private readonly DataContext context;
+
+        public NpvProfileRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<NpvProfile> GetAll()
+        {
+            var list = context.NpvProfiles
+                        .Include(_ => _.CashFlows)
+                        .Include(_ => _.NPVs)
+                        .ToList();
+
+            return list;
+        }
+
+        public IEnumerable<NpvProfile> GetById(int id)
+        {
+            var list = context.NpvProfiles.Where(_ => _.Id == id)
+                        .Include(_ => _.CashFlows)
+                        .Include(_ => _.NPVs)
+                        .ToList();
+
+            return list;
+        }
+
+        public NpvProfile Add(NpvProfile newProfile)
+        {
+            context.Add(newProfile);
+            context.SaveChanges();
+
+            return newProfile;
+        }
+
+        public NpvProfile Update(NpvProfile newProfile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
